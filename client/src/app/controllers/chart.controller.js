8cce41f07c1ff8f60cb0dde803949b029419128d
balaji a r <balaji.alambadi-rajasekar@hpe.com>
2017-06-11 19:22:05 -0500

    Feat(module-client):Added final analysis changes

--- client/src/app/controllers/chart.controller.js
+++ client/src/app/controllers/chart.controller.js
@@ -19,23 +19,43 @@
             var day = date.getDate();
             var month = date.getMonth() + 1;
             var year = date.getUTCFullYear();
-            return month+ '-' + day + '-' + year + '-' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            return month + '-' + day + '-' + year + '-' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        }
+
+        function setData(readings, xData, yData) {
+            angular.forEach(readings, function (reading) {
+                xData.push(convertDate(reading.timestamp));
+                yData[0].data.push(reading.fuelVolume);
+                yData[1].data.push(reading.engineRpm);
+                yData[2].data.push(reading.speed);
+                yData[3].data.push(reading.engineHp);
+            });
+
+            chartVm.lineChartYData = yData;
+            chartVm.lineChartXData = xData;
+
+            if (xData.length > 0) {
+                chartVm.showChart = true;
+                chartVm.showError = false;
+            } else {
+                chartVm.showChart = false;
+                chartVm.showError = true;
+            }
         }
 
         function init() {
-            chartVm.filter ={"name":"last1d"};
+            chartVm.filter = {"name": "last1d"};
+            chartVm.showChart = false;
+            chartVm.showError = false;
             chartService
                 .getReadingsByVin($routeParams.id, chartVm.filter.name)
                 .then(function (readings) {
                     var xData = [],
-                        yData = [{"name": "Fuel Volume", "data": []}, {"name": "Engine Rpm", "data": []}];
-                    angular.forEach(readings, function (reading) {
-                        xData.push(convertDate(reading.timestamp));
-                        yData[0].data.push(reading.fuelVolume);
-                        yData[1].data.push(reading.engineRpm);
-                    });
-                    chartVm.lineChartYData = yData;
-                    chartVm.lineChartXData = xData;
+                        yData = [{"name": "Fuel Volume", "data": []},
+                            {"name": "Engine Rpm", "data": []},
+                            {"name": "Speed", "data": []},
+                            {"name": "Engine Horse Power", "data": []}];
+                    setData(readings, xData, yData);
                 }, function (error) {
                     console.log(error);
                 });
@@ -46,14 +66,11 @@
                 .getReadingsByVin($routeParams.id, filter)
                 .then(function (readings) {
                     var xData = [],
-                        yData = [{"name": "Fuel Volume", "data": []}, {"name": "Engine Rpm", "data": []}];
-                    angular.forEach(readings, function (reading) {
-                        xData.push(convertDate(reading.timestamp));
-                        yData[0].data.push(reading.fuelVolume);
-                        yData[1].data.push(reading.engineRpm);
-                    });
-                    chartVm.lineChartYData = yData;
-                    chartVm.lineChartXData = xData;
+                        yData = [{"name": "Fuel Volume", "data": []},
+                            {"name": "Engine Rpm", "data": []},
+                            {"name": "Speed", "data": []},
+                            {"name": "Engine Horse Power", "data": []}];
+                    setData(readings, xData, yData);
                 }, function (error) {
                     console.log(error);
                 });
